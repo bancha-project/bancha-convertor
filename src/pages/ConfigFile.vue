355af27a41096840f1e547ea89e0ca9c847f3eaf
ahyaemon <iamahyaemon@gmail.com>
2018-10-27 11:44:03 +0900

    json yaml の変換を追加

--- src/pages/ConfigFile.vue
+++ src/pages/ConfigFile.vue
@@ -2,20 +2,30 @@
     <div>
         <div class="row">
             <form class="col s12">
-                <h5>json</h5>
-                <div class="row">
-                    <div class="input-field col s12 teal lighten-5">
-                        <textarea id="textarea1" class="materialize-textarea" style="height:200px;" v-model="jsonText" v-on:keyup="jsonChanged"></textarea>
+                <div class="config-text-title">
+                    <h5>json</h5>
+                    <button title="jsonをクリップボードにコピーします" @click="copyJson">
+                        <i class="material-icons">content_copy</i>
+                    </button>
+                </div>
+                <div class="row" id="a">
+                    <div id="jsondiv" class="input-field col s12 lighten-5" :class="{ teal: parsed, pink: !parsed }">
+                        <textarea id="jsonarea" class="materialize-textarea" v-model="jsonText" v-on:keyup="jsonChanged"></textarea>
                     </div>
                 </div>
             </form>
         </div>
         <div class="row">
             <form class="col s12">
-                <h5>yaml</h5>
+                <div class="config-text-title">
+                    <h5>yaml</h5>
+                    <button title="yamlをクリップボードにコピーします" @click="copyYaml">
+                        <i class="material-icons">content_copy</i>
+                    </button>
+                </div>
                 <div class="row">
-                    <div class="input-field col s12 teal lighten-5">
-                        <textarea id="textarea1" class="materialize-textarea" style="height:200px;" v-model="yamlText" v-on:keyup="yamlChanged"></textarea>
+                    <div id="yamldiv" class="input-field col s12 lighten-5" :class="{ teal: parsed, pink: !parsed }">
+                        <textarea id="yamlarea" class="materialize-textarea" v-model="yamlText" v-on:keyup="yamlChanged"></textarea>
                     </div>
                 </div>
             </form>
@@ -28,33 +38,114 @@
     import Vue from 'vue'
     import Component from 'vue-class-component'
     import jsyaml from "js-yaml"
+    import M from "materialize-css"
 
     @Component
     export default class ConfigFile extends Vue {
+        parsed = true
+
+        jsonElem!: Element
+        jsonDiv!: Node
         jsonText = ""
+
+        yamlElem!: Element
+        yamlDiv!: Node
         yamlText = ""
 
-        jsonChanged() {
-            let json = ""
-            try {
-                json = JSON.parse(this.jsonText)
-                this.yamlText = jsyaml.dump(json, {
-                    "indent": 4
-                })
-            } catch(err) {
+        mounted() {
+            this.jsonElem = document.getElementById("jsonarea") as Element
+            this.jsonDiv = document.getElementById("jsondiv") as Node
 
+            this.yamlElem = document.getElementById("yamlarea") as Element
+            this.yamlDiv = document.getElementById("yamldiv") as Node
+        }
+
+        jsonChanged() {
+            // 空文字の場合はパースできたとみなす
+            if(this.jsonText == "") {
+                this.yamlText = ""
+                this.parsed = true
+                return
             }
+
+            // json形式のテキストから、オブジェクトに変換
+            let obj
+            try {
+                obj = JSON.parse(this.jsonText)
+            } catch(err) {
+                console.log("jsonのパースに失敗")
+                this.parsed = false
+                return
+            }
+
+            // オブジェクトから、yaml形式のテキストに変換
+            this.yamlText = jsyaml.dump(obj, {
+                "indent": 4
+            })
+            this.parsed = true
+            M.textareaAutoResize(this.yamlElem)
         }
 
         yamlChanged() {
-            const yaml = jsyaml.load(this.yamlText)
-            const jsonText = JSON.stringify(yaml, null, 4)
-            try{
-                JSON.parse(jsonText)
-                this.jsonText = jsonText
-            } catch(err) {
-
+            // 空文字の場合はパースできたとみなす
+            if(this.yamlText == "") {
+                this.jsonText = ""
+                this.parsed = true
+                return
             }
+
+            // yaml形式のテキストから、オブジェクトに変換
+            let obj
+            try {
+                obj = jsyaml.load(this.yamlText)
+            } catch(err) {
+                console.log("yamlのパースに失敗")
+                this.parsed = false
+                return
+            }
+
+            // オブジェクトから、json形式のテキストに変換
+            this.jsonText = JSON.stringify(obj, null, 4)
+            this.parsed = true
+            M.textareaAutoResize(this.jsonElem)
         }
+
+        copyJson() {
+            document.getSelection().selectAllChildren(this.jsonDiv)
+            document.execCommand("copy")
+        }
+
+        copyYaml() {
+            document.getSelection().selectAllChildren(this.yamlDiv)
+            document.execCommand("copy")
+        }
+
     }
 </script>
+
+<style lang="scss" scoped>
+    textarea {
+        height: 200px;
+    }
+
+    .config-text-title {
+        h5 {
+            display: inline;
+        }
+
+        i {
+            display: inline;
+        }
+
+        button {
+            border: none;
+            margin-left: 10px;
+            background-color: rgba(0, 0, 0, 0);
+        }
+
+        button:hover {
+            color: rgba(158, 45, 45, 0.8);
+        }
+
+    }
+</style>
